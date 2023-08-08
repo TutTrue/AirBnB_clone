41dd1af5b0597d390332018123087a829ae83e98
sarahassan0 <sarraree5@gmail.com>
2023-08-08 22:47:57 +0300

    BaseModel: creation of the BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,38 @@
+#!usr/bin/python3
+
+""" Base model """
+import datetime
+import uuid
+
+class BaseModel():
+    """ BaseModel class """
+    
+    def __init__(self, *args, **kwargs):
+
+        if not kwargs:
+            self.id= str(uuid.uuid4())
+            self.updated_at= self.created_at=datetime.datetime.now()
+        else:
+            for key, val in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key,datetime.datetime.fromisoformat(val))
+                    else:
+                        setattr(self, key, val)
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        
+            
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+        
+    def to_dict(self):
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        
+        return my_dict
+        
+        
+        
