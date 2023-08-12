383c9d7d6dc202465ff1850fc7af65a6b956f67c
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-08-12 12:25:01 +0300

    import the classes

--- console.py
+++ console.py
@@ -7,6 +7,13 @@
 import cmd
 from models import storage
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
 
 
 class HBNBCommand(cmd.Cmd):
