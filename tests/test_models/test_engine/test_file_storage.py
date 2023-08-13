386390898053b6f8b8c5b5f9f4c340bccee93d57
sarahassan0 <sarraree5@gmail.com>
2023-08-13 21:09:34 +0300

    fix test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,4 +1,66 @@
 #!/usr/bin/python3
+"""Unittest for FileStorage model
+"""
+import unittest
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+import models
+from models.engine.file_storage import FileStorage
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+import os
+import unittest
+
+
+class Test_FileStorage_model(unittest.TestCase):
+    """
+    test class for the FileStorage model.
+    """
+
+    def test_default_values(self):
+        """
+        test the default values for the FileStorage model
+        os.remove("file.json")
+        """
+        storage = FileStorage()
+        # self.assertEqual(storage._file_path, "file.json")
+        # self.assertIsInstance(storage._file_path, str)
+        # self.assertEqual(storage.all(), {})
+        
+        self.assertIsInstance(storage.all(), dict)
+        # print(storage.reload())
+        # print(storage.all()['Place.95c566ce-3d9b-4bfe-993c-29889bb29c65'])
+        # self.assertEqual(storage.reload(), storage.all())
+        
+        
+        base = BaseModel()
+        base.save()
+        objs = storage.all()
+        obj =objs[f"{base.__class__.__name__}.{base.id}"]        
+        self.assertEqual(base, obj)
+
+        self.assertIsInstance(models.storage , FileStorage)
+        """"
+            test save class method
+        """
+        before_update_time = base.updated_at
+        base.my_number = 90
+        base.save()
+        after_update_time = base.updated_at
+        self.assertNotEqual(before_update_time, after_update_time)
+        new_number = obj.my_number
+        self.assertEqual(new_number, 90)
+
+
+
+
 """Unittest for base model
 """
 import unittest
