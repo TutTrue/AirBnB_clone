ce26a051732514c955fac2a5b389f76888d1a023
sarahassan0 <sarraree5@gmail.com>
2023-08-13 21:31:38 +0300

    unittest

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,58 +11,48 @@ class TestBaseModel(unittest.TestCase):
     """
     test class for the max_integer() function.
     """
-    base = BaseModel()
-    base.name = "My First Model"
-    base.my_number = 89
+    b = BaseModel()
+    b.name = "My First Model"
+    b.my_number = 89
 
     def test_create_instance_without_kwargs(self):
         """
         create an instance of class without kwargs
         """
-        self.assertIsInstance(self.base, BaseModel)
-        self.assertIsInstance(self.base.id, str)
-        self.assertIsInstance(self.base.created_at, datetime)
-        self.assertIsInstance(self.base.updated_at, datetime)
-        self.assertEqual(self.base.name, "My First Model")
-        self.assertEqual(self.base.my_number, 89)
+        self.assertIsInstance(self.b, BaseModel)
+        self.assertIsInstance(self.b.id, str)
+        self.assertIsInstance(self.b.created_at, datetime)
+        self.assertIsInstance(self.b.updated_at, datetime)
+        self.assertEqual(self.b.name, "My First Model")
+        self.assertEqual(self.b.my_number, 89)
 
     def test_create_instance_with_kwargs(self):
         """
         create an instance of class using kwargs
         """
-        my_base_json = self.base.to_dict()
-        new_base = BaseModel(**my_base_json)
-        self.assertIsInstance(new_base, BaseModel)
-        self.assertIsInstance(new_base.id, str)
-        self.assertIsInstance(new_base.created_at, datetime)
-        self.assertIsInstance(new_base.updated_at, datetime)
-        self.assertEqual(new_base.name, "My First Model")
-        self.assertEqual(new_base.my_number, 89)
-        self.assertNotEqual(new_base, self.base)
-        self.assertDictEqual(new_base.__dict__, self.base.__dict__)
+        my_base_json = self.b.to_dict()
+        new_b = BaseModel(**my_base_json)
+        self.assertIsInstance(new_b, BaseModel)
+        self.assertIsInstance(new_b.id, str)
+        self.assertIsInstance(new_b.created_at, datetime)
+        self.assertIsInstance(new_b.updated_at, datetime)
+        self.assertEqual(new_b.name, "My First Model")
+        self.assertEqual(new_b.my_number, 89)
+        self.assertNotEqual(new_b, self.b)
+        self.assertDictEqual(new_b.__dict__, self.b.__dict__)
 
-    def test_to_dict(self):
-        """
-            test to_dict class method
-        """
-        to_dict_returned_dict = self.base.to_dict()
-        expected_dic = self.base.__dict__.copy()
-        expected_dic["__class__"] = self.base.__class__.__name__
-        expected_dic["updated_at"] = self.base.updated_at.isoformat()
-        expected_dic["created_at"] = self.base.created_at.isoformat()
-        self.assertDictEqual(expected_dic, to_dict_returned_dict)
 
     def test_save(self):
         """"
             test save class method
         """
-        before_update_time = self.base.updated_at
-        self.base.my_number = 90
-        self.base.save()
-        after_update_time = self.base.updated_at
+        before_update_time = self.b.updated_at
+        self.b.my_number = 90
+        self.b.save()
+        after_update_time = self.b.updated_at
         self.assertNotEqual(before_update_time, after_update_time)
         all_objects = storage.all()
-        obj = all_objects[f"{self.base.__class__.__name__}.{self.base.id}"]
+        obj = all_objects[f"{self.b.__class__.__name__}.{self.b.id}"]
         new_number = obj.my_number
         self.assertEqual(new_number, 90)
 
@@ -72,5 +62,16 @@ class TestBaseModel(unittest.TestCase):
 
             check for string representaion
         """
-        expected_str = f"[{self.base.__class__.__name__}] ({self.base.id}) {self.base.__dict__}"
-        self.assertEqual(self.base.__str__(), expected_str)
+        s = f"[{self.b.__class__.__name__}] ({self.b.id}) {self.b.__dict__}"
+        self.assertEqual(self.b.__str__(), s)
+
+    def test_to_dict(self):
+        """
+            test to_dict class method
+        """
+        to_dict_returned_dict = self.b.to_dict()
+        expected_dic = self.b.__dict__.copy()
+        expected_dic["__class__"] = self.b.__class__.__name__
+        expected_dic["updated_at"] = self.b.updated_at.isoformat()
+        expected_dic["created_at"] = self.b.created_at.isoformat()
+        self.assertDictEqual(expected_dic, to_dict_returned_dict)
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Unittest for FileStorage model
 """
+from datetime import datetime
 import unittest
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
@@ -30,23 +31,14 @@ class Test_FileStorage_model(unittest.TestCase):
         os.remove("file.json")
         """
         storage = FileStorage()
-        # self.assertEqual(storage._file_path, "file.json")
-        # self.assertIsInstance(storage._file_path, str)
-        # self.assertEqual(storage.all(), {})
-        
         self.assertIsInstance(storage.all(), dict)
-        # print(storage.reload())
-        # print(storage.all()['Place.95c566ce-3d9b-4bfe-993c-29889bb29c65'])
-        # self.assertEqual(storage.reload(), storage.all())
-        
-        
         base = BaseModel()
         base.save()
         objs = storage.all()
-        obj =objs[f"{base.__class__.__name__}.{base.id}"]        
+        obj = objs[f"{base.__class__.__name__}.{base.id}"]
         self.assertEqual(base, obj)
 
-        self.assertIsInstance(models.storage , FileStorage)
+        self.assertIsInstance(models.storage, FileStorage)
         """"
             test save class method
         """
@@ -59,40 +51,30 @@ class Test_FileStorage_model(unittest.TestCase):
         self.assertEqual(new_number, 90)
 
 
-
-
-"""Unittest for base model
-"""
-import unittest
-from models.base_model import BaseModel
-from models import storage
-from datetime import datetime
-
-
 class TestBaseModel(unittest.TestCase):
     """
     test class for the max_integer() function.
     """
-    base = BaseModel()
-    base.name = "My First Model"
-    base.my_number = 89
+    b = BaseModel()
+    b.name = "My First Model"
+    b.my_number = 89
 
     def test_create_instance_without_kwargs(self):
         """
         create an instance of class without kwargs
         """
-        self.assertIsInstance(self.base, BaseModel)
-        self.assertIsInstance(self.base.id, str)
-        self.assertIsInstance(self.base.created_at, datetime)
-        self.assertIsInstance(self.base.updated_at, datetime)
-        self.assertEqual(self.base.name, "My First Model")
-        self.assertEqual(self.base.my_number, 89)
+        self.assertIsInstance(self.b, BaseModel)
+        self.assertIsInstance(self.b.id, str)
+        self.assertIsInstance(self.b.created_at, datetime)
+        self.assertIsInstance(self.b.updated_at, datetime)
+        self.assertEqual(self.b.name, "My First Model")
+        self.assertEqual(self.b.my_number, 89)
 
     def test_create_instance_with_kwargs(self):
         """
         create an instance of class using kwargs
         """
-        my_base_json = self.base.to_dict()
+        my_base_json = self.b.to_dict()
         new_base = BaseModel(**my_base_json)
         self.assertIsInstance(new_base, BaseModel)
         self.assertIsInstance(new_base.id, str)
@@ -100,31 +82,31 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(new_base.updated_at, datetime)
         self.assertEqual(new_base.name, "My First Model")
         self.assertEqual(new_base.my_number, 89)
-        self.assertNotEqual(new_base, self.base)
-        self.assertDictEqual(new_base.__dict__, self.base.__dict__)
+        self.assertNotEqual(new_base, self.b)
+        self.assertDictEqual(new_base.__dict__, self.b.__dict__)
 
     def test_to_dict(self):
         """
             test to_dict class method
         """
-        to_dict_returned_dict = self.base.to_dict()
-        expected_dic = self.base.__dict__.copy()
-        expected_dic["__class__"] = self.base.__class__.__name__
-        expected_dic["updated_at"] = self.base.updated_at.isoformat()
-        expected_dic["created_at"] = self.base.created_at.isoformat()
+        to_dict_returned_dict = self.b.to_dict()
+        expected_dic = self.b.__dict__.copy()
+        expected_dic["__class__"] = self.b.__class__.__name__
+        expected_dic["updated_at"] = self.b.updated_at.isoformat()
+        expected_dic["created_at"] = self.b.created_at.isoformat()
         self.assertDictEqual(expected_dic, to_dict_returned_dict)
 
     def test_save(self):
         """"
             test save class method
         """
-        before_update_time = self.base.updated_at
-        self.base.my_number = 90
-        self.base.save()
-        after_update_time = self.base.updated_at
+        before_update_time = self.b.updated_at
+        self.b.my_number = 90
+        self.b.save()
+        after_update_time = self.b.updated_at
         self.assertNotEqual(before_update_time, after_update_time)
         all_objects = storage.all()
-        obj = all_objects[f"{self.base.__class__.__name__}.{self.base.id}"]
+        obj = all_objects[f"{self.b.__class__.__name__}.{self.b.id}"]
         new_number = obj.my_number
         self.assertEqual(new_number, 90)
 
@@ -134,5 +116,5 @@ class TestBaseModel(unittest.TestCase):
 
             check for string representaion
         """
-        expected_str = f"[{self.base.__class__.__name__}] ({self.base.id}) {self.base.__dict__}"
-        self.assertEqual(self.base.__str__(), expected_str)
+        s = f"[{self.b.__class__.__name__}] ({self.b.id}) {self.b.__dict__}"
+        self.assertEqual(self.b.__str__(), s)
